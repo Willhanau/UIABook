fb1c7e9ac8741a50bcc99b0469a63dd0681cc200
willhanau <willhanau@gmail.com>
2016-02-18 14:54:15 -0800

    On page 38

---
+++ UnityInActionBook/Assets/Scripts/MouseLook.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MouseLook : MonoBehaviour {
+
+	public enum RotationAxes
+	{
+		MouseXAndY = 0,
+		MouseX = 1,
+		MouseY = 2
+	}
+
+	public RotationAxes axes = RotationAxes.MouseXAndY;
+	public float SensitivityHori = 9.0f;
+	public float SensitivityVert = 9.0f;
+
+	public float minimumVert = -45.0f;
+	public float maximumVert = 45.0f;
+
+	private float _rotationX = 0;
+
+	void Update () {
+		if (axes == RotationAxes.MouseX){
+			//horizontal rotation here
+			transform.Rotate(0, Input.GetAxis("Mouse X") * SensitivityHori, 0);
+		}
+		else if (axes == RotationAxes.MouseY){
+			//vertical rotation here
+			_rotationX -= Input.GetAxis("Mouse Y") * SensitivityVert;
+			_rotationX = Mathf.Clamp (_rotationX, minimumVert, maximumVert);
+
+			float _rotationY = transform.localEulerAngles.y;
+			transform.localEulerAngles = new Vector3 (_rotationX, _rotationY, 0);
+
+		}
+		else {
+			//both horizontal and vertical rotation here
+		}
+	}
+
+
+
+}
---
+++ UnityInActionBook/Assets/Scripts/Spin.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Spin : MonoBehaviour {
+
+	public int speed = 3;
+
+	void Update () {
+		transform.Rotate (0, speed, 0);
+	}
+}
