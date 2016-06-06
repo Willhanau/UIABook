366a87251ac04a9506f13016538b764a0a15ed63
willhanau <willhanau@gmail.com>
2016-06-05 19:02:55 -0700

    On page 64

---
+++ UnityInActionBook/Assets/Scripts/ReactiveTarget.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ReactiveTarget : MonoBehaviour {
+
+	// Use this for initialization
+	public void ReactToHit(){
+		WanderingAI behavior = GetComponent<WanderingAI> ();
+		if (behavior != null && behavior.GetAlive()) {
+			StartCoroutine (Die ());
+			behavior.SetAlive (false);
+		}
+
+	}
+
+	private IEnumerator Die(){
+		this.transform.Rotate (-75, 0, 0);
+
+		yield return new WaitForSeconds (1.5f);
+
+		Destroy (this.gameObject);
+	}
+
+}
